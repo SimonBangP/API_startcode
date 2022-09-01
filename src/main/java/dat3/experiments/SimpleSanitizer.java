e0d4c627586cf5b78d5aaca251291d65699b2a52
Simon Bang <simo12k1@stud.kea.dk>
2022-09-01 12:27:28 +0200

    Startet på Member API.

---
+++ src/main/java/dat3/experiments/SimpleSanitizer.java
@@ -0,0 +1,11 @@
+package dat3.experiments;
+
+public class SimpleSanitizer {
+    public static String simpleSanitize(String s){
+        //throw new UnsupportedOperationException("Not implemented yet");
+
+
+        return s.replaceAll("\\<[^>]*>","");
+    }
+
+}
