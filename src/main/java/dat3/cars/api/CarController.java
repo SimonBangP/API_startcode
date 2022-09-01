4c98e9400d74755c9409f2cae9069dfe0f709c2d
Simon Bang <simo12k1@stud.kea.dk>
2022-09-01 14:27:15 +0200

    Startet på Car API

---
+++ src/main/java/dat3/cars/api/CarController.java
@@ -0,0 +1,13 @@
+package dat3.cars.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cars")
+public class CarController {
+
+
+
+}
