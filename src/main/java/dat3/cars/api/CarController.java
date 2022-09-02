8b1329fec99ea95007a4b6edc44891eb5fbdcfb8
Simon Bang <simo12k1@stud.kea.dk>
2022-09-02 11:32:31 +0200

    Startet på Car API

--- src/main/java/dat3/cars/api/CarController.java
+++ src/main/java/dat3/cars/api/CarController.java
@@ -3,6 +3,7 @@ package dat3.cars.api;
 
 import dat3.cars.dto.CarResponse;
 import dat3.cars.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,12 @@ import java.util.List;
 @RequestMapping("api/cars")
 public class CarController {
 
+   // @Autowired
 CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping
     public List<CarResponse> getCars (){
--- src/main/java/dat3/cars/service/CarService.java
+++ src/main/java/dat3/cars/service/CarService.java
@@ -3,20 +3,26 @@ package dat3.cars.service;
 import dat3.cars.dto.CarResponse;
 import dat3.cars.entity.Car;
 import dat3.cars.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CarService {
-
     private CarRepository carRepository;
+
+    public CarService (CarRepository carRepository){
+        this.carRepository = carRepository;
+    }
 
 
 public List<CarResponse> getCars (){
 
     List<Car> cars = carRepository.findAll();
-    List<CarResponse> response = cars.stream().map(car -> new CarResponse(car, false)).collect(Collectors.toList());
 
+    List<CarResponse> response = cars.stream().map(car -> new CarResponse(car, false)).collect(Collectors.toList());
     return response;
 }
 }
